c257f011c7b7569120bb030ade03a9e31383ed15
Raj <rawatraj995@gmail.com>
2023-09-01 18:01:40 +0530

    feat:community model & post Threads done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,13 @@
 import { UserButton } from "@clerk/nextjs";
  
-export default function Home() {
+export default async function Home() {
+
+  const result= await fetchPosts();
+
   return (
     <div>
      <h1 className="head-text text-left">Home</h1>
+
     </div>
   )
 }
