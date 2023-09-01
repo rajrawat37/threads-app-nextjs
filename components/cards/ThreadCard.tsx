2dc0fd24de372f7a76eef863036c6a6b7f7de643
Raj <rawatraj995@gmail.com>
2023-09-01 21:57:25 +0530

    fix:share icons

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -87,7 +87,7 @@ function ThreadCard({
                     className='cursor-pointer object-contain'
                   />
                 </Link>
-                {/* <Image
+                 <Image
                   src='/assets/repost.svg'
                   alt='heart'
                   width={24}
@@ -100,7 +100,7 @@ function ThreadCard({
                   width={24}
                   height={24}
                   className='cursor-pointer object-contain'
-                /> */}
+                /> 
               </div>
 
               {isComment && comments.length > 0 && (
