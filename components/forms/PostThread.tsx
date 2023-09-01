003be9a844c60ebe196c27886d58fec978c1a93d
Raj <rawatraj995@gmail.com>
2023-09-01 16:32:00 +0530

    feat:create-threads

---
+++ components/forms/PostThread.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+interface Props {
+  userId: string;
+}
+
+function PostThread({ userId }: Props) {
+    return(
+        <h1>Post Threads</h1>
+    )
+}
+
+export default PostThread;
